4b288ab805fcf3982511a0e4c1d758202bd00558
Alauddin710 <mdalauddinalo710@gmail.com>
2022-08-06 15:04:01 +0600

    Add File

--- PHP/4Aug2022/getmxrr.php
+++ PHP/4Aug2022/getmxrr.php
@@ -1,6 +1,6 @@
 <?php 
 echo "<pre>";
-getmxrr("prothomalo.com", $mxhosts);
+getmxrr("prothomalo.com", $mxhosts); // er maddome jana jai bibinno compani ki hosts babor korse
 getmxrr("prothomalo.com", $mxhosts);
 print_r($mxhosts)
 ?>
