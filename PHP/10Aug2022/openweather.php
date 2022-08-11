018da975819643b3810a82e0fbabc4dd7abcd2ae
Alauddin710 <mdalauddinalo710@gmail.com>
2022-08-11 13:10:18 +0600

    Add file

--- PHP/10Aug2022/openweather.php
+++ PHP/10Aug2022/openweather.php
@@ -1,9 +1,9 @@
 <?php
 $OpenWeather = ['api_key' => '1bad80fc7220d906e8b5950e94759beb'];
-$zip = "98109";
-$lat= 23.7700761;
-$lon = 90.442869;
-$base_url = "https://api.openweathermap.org/data/2.5";
+$zip = "7050"; // akane zipcode dite hobe jekankar weather dekte hobe
+$lat= 23.96723658880676 ; // aigulo niea aste hobe google map theke right button click kore number gulo
+$lon =88.93451149722618;
+$base_url = "https://api.openweathermap.org/data/2.5";// upoer api key niea aste hobe ai website accout khule
 $weather_url = "/weather?lat=" . $lat."&lon=" .$lon;
 $api_key = "&appid={$OpenWeather['api_key']}";
 $api_url = $base_url . $weather_url . $api_key;
