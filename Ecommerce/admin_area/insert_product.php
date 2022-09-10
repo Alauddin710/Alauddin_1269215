76523f7884604ad539930919b0b06c3eb5434a95
Alauddin710 <mdalauddinalo710@gmail.com>
2022-09-10 13:33:12 +0600

    Home Add

---
+++ Ecommerce/admin_area/insert_product.php
@@ -0,0 +1,92 @@
+<?php include('../includes/connect.php') ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>insert_product Admin-dashboard</title>
+    <!-- Bootstrap Css link -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- font awesome link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- css file -->
+    <link rel="stylesheet" href="../style.css">
+</head>
+<body class="bg-light">
+    <div class="container mt-3 ">
+        <h1 class="text-center">Insert Product</h1>
+        <!-- form -->
+        <form action="" method="post" enctype="multipart/form-data">
+            <!-- title -->
+            <div class="form-outline mb-4 w-50 m-auto">
+               <label for="product_title" class="form-label">Product Title</label>
+               <input type="text" name="product_title" id="product_title" class="form-control" placeholder="Enter prodcut title" autocomplete="off" required="required">
+            </div>
+            <!-- description -->
+            <div class="form-outline mb-4 w-50 m-auto">
+               <label for="product_title" class="form-label">Product Description</label>
+               <input type="text" name="description" id="description" class="form-control" placeholder="Enter prodcut Description" autocomplete="off" required="required">
+            </div>
+            <!-- keywords -->
+            <div class="form-outline mb-4 w-50 m-auto">
+               <label for="product_keywords" class="form-label">Product Keywords</label>
+               <input type="text" name="product_keywords" id="product_keywords" class="form-control" placeholder="Enter product Keywords" autocomplete="off" required="required">
+            </div>
+            <!-- Categories -->
+            <div class="form-outline mb-4 w-50 m-auto">
+               <select name="prodcut_category" id="" class="form-select">
+                <option value="">Select a Category</option>
+                <?php 
+                $select_query= "Select * from `categories`";
+                $result_query = mysqli_query($con,$select_query);
+                while($row=mysqli_fetch_assoc($result_query)){
+                    $category_title = $row['category_title'];
+                    $category_id = $row['category_id'];
+                    echo "<option value=' $category_id'> $category_title</option>";
+                }
+
+                ?>
+                
+            </div>
+
+             <!-- Brands -->
+             <div class="form-outline mb-4 w-50 m-auto">
+               <select name="product_brands" id="" class="form-select">
+                <option value="">Select a Brands</option>
+                <option value="">Brands1</option>
+                <option value="">Brands2</option>
+                <option value="">Brands3</option>
+                <option value="">Brands4</option>
+               </select>
+            </div>
+            
+             <!-- Image 1 -->
+             <div class="form-outline mb-4 w-50 m-auto">
+               <label for="product_image1" class="form-label">Product image1</label>
+               <input type="file" name="product_image1" id="product_image1" class="form-control" required="required">
+            </div>
+             <!-- Image 2 -->
+             <div class="form-outline mb-4 w-50 m-auto">
+               <label for="product_image2" class="form-label">Product image2</label>
+               <input type="file" name="product_image2" id="product_image2" class="form-control" required="required">
+            </div>
+             <!-- Image 1 -->
+             <div class="form-outline mb-4 w-50 m-auto">
+               <label for="product_image3" class="form-label">Product image3</label>
+               <input type="file" name="product_image3" id="product_image3" class="form-control" required="required">
+            </div>
+             <!-- Price -->
+             <div class="form-outline mb-4 w-50 m-auto">
+               <label for="product_price" class="form-label">Product Price</label>
+               <input type="text" name="product_price" id="product_price" class="form-control" placeholder="Enter product Price" autocomplete="off" required="required">
+            </div>
+             <!-- Price -->
+             <div class="form-outline mb-4 w-50 m-auto">
+               <input type="submit" name="insert_product" class="btn btn-info mb-3 px-3" value="Insert Products">
+            </div>
+        </form>
+    </div>
+    
+</body>
+</html>
---
+++ Ecommerce/mystore.sql
@@ -0,0 +1,107 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 10, 2022 at 09:31 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 7.4.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mystore`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `brands`
+--
+
+CREATE TABLE `brands` (
+  `brand_id` int(11) NOT NULL,
+  `brand_title` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `brands`
+--
+
+INSERT INTO `brands` (`brand_id`, `brand_title`) VALUES
+(1, 'swigy'),
+(2, 'zomato'),
+(3, 'mc donald'),
+(4, 'alabadi'),
+(5, 'nike'),
+(6, 'amazone'),
+(7, 'filipcart');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `category_id` int(11) NOT NULL,
+  `category_title` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`category_id`, `category_title`) VALUES
+(1, 'fruits'),
+(2, 'juice'),
+(3, 'Vegetable'),
+(4, 'milk products'),
+(5, 'books'),
+(6, 'Chips'),
+(7, 'ice cream');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `brands`
+--
+ALTER TABLE `brands`
+  ADD PRIMARY KEY (`brand_id`);
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`category_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `brands`
+--
+ALTER TABLE `brands`
+  MODIFY `brand_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
