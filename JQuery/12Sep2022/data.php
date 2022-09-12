f6a32557c7a86bf141188e459eda0a8d50755225
Alauddin710 <mdalauddinalo710@gmail.com>
2022-09-12 19:15:25 +0600

    Add File

---
+++ JQuery/12Sep2022/data.php
@@ -0,0 +1,18 @@
+
+<?php 
+echo "Hello world,How are your? How your project going"; 
+echo "Hello world,How are your? How your project going"; 
+echo "Hello world,How are your? How your project going"; 
+echo "Hello world,How are your? How your project going"; 
+echo "Hello world,How are your? How your project going"; 
+echo "Hello world,How are your? How your project going"; 
+echo "Hello world,How are your? How your project going"; 
+echo "Hello world,How are your? How your project going"; 
+echo "Hello world,How are your? How your project going"; 
+echo "Hello world,How are your? How your project going"; 
+echo "Hello world,How are your? How your project going"; 
+echo "Hello world,How are your? How your project going"; 
+echo "Hello world,How are your? How your project going"; 
+echo "Hello world,How are your? How your project going"; 
+
+?> 
