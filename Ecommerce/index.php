e178b3a1f6a172ce6451f9bf634442664a1f7cbb
Alauddin710 <mdalauddinalo710@gmail.com>
2022-09-13 13:21:20 +0600

    Home Add

--- Ecommerce/index.php
+++ Ecommerce/index.php
@@ -1,6 +1,7 @@
 <!-- connect file -->
 <?php
-include('includes/connect.php')
+include('includes/connect.php');
+include('functions/common_function.php')
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -82,32 +83,10 @@ include('includes/connect.php')
     <div class="row">
       <!-- fetching products -->
       <?php 
-      $select_query= "select * from `products` order by rand() LIMIT 0,9";
-      $result_query = mysqli_query($con,$select_query);
-      // $row= mysqli_fetch_assoc( $result_query);
-      // echo $row['product_title']
-      while($row=mysqli_fetch_assoc($result_query)){
-        $product_id = $row['product_id'];
-        $product_title = $row['product_title'];
-        $product_description = $row['product_description'];
-        $product_image1 = $row['product_image1'];
-        $product_price = $row['product_price'];
-        $category_id = $row['category_id'];
-        $brand_id = $row['brand_id'];
-        // echo  $product_title;
-        // echo "<br>";
-        echo " <div class='col-md-4 mb-2'>
-        <div class='card' style='width: 18rem;'>
-              <img src='./admin_area/product_images/$product_image1' class='card-img-top' alt='$product_title'>
-              <div class='card-body'>
-                <h5 class='card-title'>$product_title</h5>
-                <p class='card-text'>$product_description.</p>
-                <a href='#' class='btn btn-info'>Add to card</a>
-                <a href='#' class='btn btn-secondary'>view more</a>
-              </div>
-        </div>
-      </div>";
-      }
+      //calling function common.php file theke
+       getproducts();
+       get_unique_categories();
+       get_unique_brands()
       ?>
       
   <!-- Row end -->
@@ -122,18 +101,8 @@ include('includes/connect.php')
       </li>
 
       <?php 
-      $select_brands = "Select * from `brands`";
-      $result_brands = mysqli_query($con, $select_brands);
-      // $row_data = mysqli_fetch_assoc($result_brands);
-      // echo $row_data['brand_title']
-      while($row_data= mysqli_fetch_assoc( $result_brands)){
-          $brand_title = $row_data['brand_title'];
-          $brand_id = $row_data['brand_id'];
-          // echo $brand_title;
-          echo "  <li class='nav-item'>
-          <a href='index.php?brand=$brand_id' class='nav-link text-light'> $brand_title</a>
-        </li>";
-      }
+      //calling function common.php file theke
+    getbrands();
       ?>
 
     </ul>
@@ -144,18 +113,9 @@ include('includes/connect.php')
       </li>
      
       <?php 
-      $select_categories = "Select * from `categories`";
-      $result_categories = mysqli_query($con, $select_categories);
-      // $row_data = mysqli_fetch_assoc($result_brands);
-      // echo $row_data['brand_title']
-      while($row_data= mysqli_fetch_assoc( $result_categories)){
-          $category_title = $row_data['category_title'];
-          $category_id = $row_data['category_id'];
-          // echo $brand_title;
-          echo "  <li class='nav-item'>
-          <a href='index.php?category=$category_id' class='nav-link text-light'> $category_title</a>
-        </li>";
-      }
+       //calling function common.php file theke
+     getcategories();
+     
       ?>
     </ul>
   </div>
---
+++ JQuery/Home_practice/12Sep2022/get_data.php
@@ -0,0 +1,6 @@
+<?php 
+$id = $_GET['id'];
+$fruits= array ("Apple","Orange","Lemon00","Banana","ruti");
+echo $fruits[2];
+
+?>
